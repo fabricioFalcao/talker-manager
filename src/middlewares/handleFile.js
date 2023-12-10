eea2c634e30816a60de8db3c9b124759616379f1
fabricioFalcao <fabriciohfd@gmail.com>
2023-12-10 13:24:30 -0300

    Requisito 1 lint

--- src/middlewares/handleFile.js
+++ src/middlewares/handleFile.js
@@ -5,13 +5,12 @@ const filePath = resolve(__dirname, '../talker.json');
 
 const readFile = async () => {
   try {
-    const data = await fs.readFile(filePath, 'utf-8')
+    const data = await fs.readFile(filePath, 'utf-8');
     return JSON.parse(data);
   } catch (error) {
     return null;
   }
 };
-
 
 module.exports = {
   readFile,
