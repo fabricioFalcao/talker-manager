0fda2ca36e25fcb27dfde60b8812506de7baea84
fabricioFalcao <fabriciohfd@gmail.com>
2023-12-10 14:45:40 -0300

    Requisito 3

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,5 +1,12 @@
 const { Router } = require('express');
+const tokenGenerator = require('../utils/tokenGenerator');
 
 const loginRouter = Router();
+
+loginRouter.post('/', (_req, res) => {
+  // const { email, password } = req.body;
+  const token = tokenGenerator();
+  res.status(200).json({ token });
+});
 
 module.exports = loginRouter;
