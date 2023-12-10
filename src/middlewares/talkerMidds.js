744758f77b1ecdf350b6abd38139df0eed12d3d2
fabricioFalcao <fabriciohfd@gmail.com>
2023-12-10 14:07:53 -0300

    Requisito 2

--- src/middlewares/talkerMidds.js
+++ src/middlewares/talkerMidds.js
@@ -2,6 +2,13 @@ const { readFile } = require('../utils/handleFile');
 
 const getAllTalkers = async () => readFile();
 
+const getTalkerById = async (id) => {
+  const talkersList = await readFile();
+  const talker = talkersList.find((element) => element.id === +id);
+  return talker;
+};
+
 module.exports = {
-  getAllTalkers
+  getAllTalkers,
+  getTalkerById,
 };
--- src/routes/talker.js
+++ src/routes/talker.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
-const { getAllTalkers } = require('../middlewares/talkerMidds');
+const { getAllTalkers, getTalkerById } = require('../middlewares/talkerMidds');
 
 const talkerRouter = Router();
 
 talkerRouter.get('/', async (_req, res) => {
   const talker = await getAllTalkers();
+  res.status(200).send(talker);
+});
+
+talkerRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await getTalkerById(id);
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   res.status(200).send(talker);
 });
 
